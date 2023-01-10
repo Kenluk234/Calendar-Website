1878a9fde8245f6365bfa73fcea579b51558c6fb
Kenluk234 <114371901+Kenluk234@users.noreply.github.com>
2023-01-10 18:33:45 +0700

    Update and rename script.js to modal.js

---
+++ modal.js
@@ -0,0 +1,29 @@
+var modal = $('#interest-modal');
+var gamers = $('#game-article1');
+var gamer = $('#game-article2');
+$('.box').hide();
+
+$('#cl_add').click(function() {
+  modal-content.show(1000);
+})
+
+$
+$('#container1').click(function() {
+    modal.hide(1000);
+    gamers.show(1000);
+    gamer.show(1000);
+});
+
+$('#container2').click(function() {
+    modal.hide(1000);
+
+});
+
+$('#container3').click(function() {
+    modal.hide(1000);
+
+});
+
+gamer.click(function() {
+    window.location = 'https://clutchpoints.com/csgos-most-expensive-ak-47-skin-will-sell-for-over-400000-and-heres-why';
+})
